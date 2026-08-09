07c8244b0b77f39eb4c684e7ec5458bb2c1f45fa
Shyam <neelameghashyam@gmail.com>
2026-08-09 10:08:28 +0530

    clean up

--- test-openai.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from "openai";
-
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const models = await client.models.list();
-
-models.data.forEach((m) => console.log(m.id));
